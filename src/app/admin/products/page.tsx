2ba72d988b5f02630bea445b620c2cb482e5c260
apykwan <apykwan@hotmail.com>
2024-07-07 04:06:57 -0700

    working on table display on product page

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -1,15 +1,38 @@
 import Link from 'next/link';
 
 import { Button } from '@/components/ui/button';
+import { Table, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 import { PageHeader } from '../_components/PageHeader';
 
 export default function AdminProductsPage() {
   return (
-    <div className="flex justify-between items-center gap-4">
-      <PageHeader>Products</PageHeader>
-      <Button>
-        <Link href="/admin/products/new">Add Product</Link>
-      </Button>
-    </div>
-  )
+    <>
+      <div className="flex justify-between items-center gap-4">
+        <PageHeader>Products</PageHeader>
+        <Button asChild>
+          <Link href="/admin/products/new">Add Product</Link>
+        </Button>
+        
+      </div>
+      <ProductsTable />
+    </>
+  );
+}
+
+function ProductsTable() {
+  return <Table>
+    <TableHeader>
+      <TableRow>
+        <TableHead className="w-0">
+          <span className="sr-only">Available for Purchase</span>
+        </TableHead>
+        <TableHead>Name</TableHead>
+        <TableHead>Price</TableHead>
+        <TableHead>Orders</TableHead>
+        <TableHead className="w-0">
+          <span className="sr-only">Actions</span>
+        </TableHead>
+      </TableRow>
+    </TableHeader>
+  </Table>
 }
